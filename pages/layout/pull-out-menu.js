a746f33b38ec15cfed0e093ed8fc3f677ed021a7
Tomoe <4.artem.0@gmail.com>
2022-06-08 19:16:12 +0300

    update: layout adaptive for mobile

--- pages/layout/pull-out-menu.js
+++ pages/layout/pull-out-menu.js
@@ -3,58 +3,43 @@ class MenuGroup {
      * @param {Element} menu
      * @param {NodeList} togglers
      */
-    constructor(menu, togglers) {
-        if (!menu) {console.warn(`MenuGroup required menuElement but passed ${menu}`); return false};
-        this.menu = menu;
+    constructor(subscribers, togglers) {
+        if (!subscribers.length || !subscribers.length) {
+            console.warn("Menu group is corrupted!");
+            console.log('constructor(subscribers ... ) ', subscribers);
+            console.log('constructor(togglers ... ) ', togglers);
+        };
+        this.subscribers = subscribers;
         this.togglers = togglers;
-
         this.menuState = false;
-        if (menu.classList.contains('hidden')) null;
-        else if (menu.classList.contains('shown')) this.menuState = true;
-
+        
         this.toggleMenuState = this.toggleMenuState.bind(this);
-
         for (let toggler of this.togglers) toggler.addEventListener('click', this.toggleMenuState);
-        this.applyMenuState(this.menu);
-        this.applyTogglersState(this.togglers);
+        this.updateElementsState()
     }
 
     toggleMenuState() {
         this.menuState = !this.menuState;
-        this.applyMenuState(this.menu);
-        this.applyTogglersState(this.togglers);
+        this.updateElementsState(this.subscribers);
+    }
+
+    updateElementsState() {
+        for (let element of this.subscribers) this.applyStateToElement(element);
+        for (let element of this.togglers) this.applyStateToElement(element);
     }
 
     /**
      * @param {Element} menu
      */
-    applyMenuState(menu) {
+    applyStateToElement(element) {
         if (this.menuState === true) {
-            menu.classList.remove('hidden');
-            menu.classList.add('shown');
+            element.dataset.state="enabled"
         } else {
-            menu.classList.remove('shown');
-            menu.classList.add('hidden');
-        }
-    }
-
-    /**
-     * 
-     * @param {NodeList} togglers 
-     */
-    applyTogglersState(togglers) {
-        for (let toggler of togglers) {
-            if (this.menuState === true) {
-                toggler.classList.remove('show');
-                toggler.classList.add('hidde');
-            } else {
-                toggler.classList.remove('hidde');
-                toggler.classList.add('show');
-            }
+            element.dataset.state="disabled"
         }
     }
 }
 
-const menu = document.querySelector(`[data-role="pull-out-menu"]`);
+const subscribers = document.querySelectorAll(`[state-subscriber="pull-out-menu"]`);
 const togglers = document.querySelectorAll(`[data-role="pull-out-menu-toggler"]`);
-new MenuGroup(menu, togglers);
+new MenuGroup(subscribers, togglers);
