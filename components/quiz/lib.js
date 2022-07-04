800ac15962c2e21cea4bc0ece1f0b2a29f6d2313
Qsppl <4.artem.0@gmail.com>
2022-07-04 19:33:53 +0300

    Update lib.js

--- components/quiz/lib.js
+++ components/quiz/lib.js
@@ -17,3 +17,27 @@ export function kebabize(string) {
 export function camelize(string) {
     return string.replace(/-./g, x=>x[1].toUpperCase());
 }
+
+'<input oninput="this.value = phoneFormate(this.value)">';
+function phoneFormate(value) {
+    let prefix = ""
+    if (value[0] === "+") prefix = "+";
+    value = value.replace(/\D/g, '').split(/(?=.)/);
+    value = value.filter((val) => {
+      val = parseInt(val);
+      return Number.isInteger(val)
+    })
+    let i = value.length;
+    if (i >= 11) value.length = 11;
+    console.log(value);
+    if (i >= 1) {
+      if (value[0] === "7" || value[0] === "8") value.splice(0, 1, 7);
+      else {value.unshift("7"); ++i}
+    }
+    if (i >= 1) value.unshift('+ ');
+    if (i >= 2) value.splice(2, 0, ' ');
+    if (i >= 5) value.splice(6, 0, ' ');
+    if (i >= 8) value.splice(10, 0, '-');
+    if (i >= 10) value.splice(13, 0, '-');
+    return value.join('') ?value.join('') : prefix;
+  }
